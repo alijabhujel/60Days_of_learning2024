f16a265940e6d8344080fd1553a6796284770229
alija-bhujel <alijabhujel111@gmail.com>
2024-06-11 23:41:33 +0545

    login

--- lib/bloc/login/login_bloc.dart
+++ lib/bloc/login/login_bloc.dart
@@ -1,12 +1,14 @@
 import 'package:bloc_state_manage/bloc/login/login_event.dart';
 import 'package:bloc_state_manage/bloc/login/login_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 class LoginBloc extends Bloc<LoginEvent, LoginStates> {
   LoginBloc() : super(LoginStates()) {
     on<LoginEvent>((event, emit) {
       on<Emailchanges>(_onemailchanges);
       on<Passwordchange>(_onpasswordchanges);
+      on<Loginapi>(_loginapi);
     });
   }
 
@@ -16,5 +18,24 @@ class LoginBloc extends Bloc<LoginEvent, LoginStates> {
 
   void _onpasswordchanges(Passwordchange event, Emitter<LoginStates> emit) {
     emit(state.copyWith(password: event.password));
+  }
+
+  void _loginapi(Loginapi event, Emitter<LoginStates> emit) async {
+    Map data = {'email': state.email, 'password': state.password};
+
+    try {
+      final response = await http
+          .post(Uri.parse('https://reqres.in/api/users?page=2'), body: data);
+      if (response.statusCode == 200) {
+        emit(state.copyWith(
+            loginstatus: Loginstatus.success, message: 'login success'));
+      } else {
+        emit(state.copyWith(
+            loginstatus: Loginstatus.error, message: 'something went wrong'));
+      }
+    } catch (e) {
+      emit(state.copyWith(
+          loginstatus: Loginstatus.error, message: e.toString()));
+    }
   }
 }
